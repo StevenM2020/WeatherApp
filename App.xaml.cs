12afd6e3f918e1004bbc51bfceaa1bb5f7150ed0
Steven <steven.bikerider@gmail.com>
2024-01-29 18:14:13 -0500

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace WeatherApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            StoreApiKey();
+
+            MainPage = new AppShell();
+
+           
+        }
+
+        private async void StoreApiKey()
+        {
+            await SecureStorage.SetAsync("ApiKey", "YOUR_API_KEY");
+        }
+
+        // this method is used to set the size of the window
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            Window window = base.CreateWindow(activationState);
+            window.Height = 600;
+            window.Width = 600;
+            return window;
+        }
+    }
+}
